7174c60530d8785e349c7a43161def76109efe4d
Ewuranna Smith-Quayson <qusmew@gmail.com>
2022-01-18 17:57:55 +0000

    responsive nav bar

---
+++ src/Components/Navbar/Menuitems.js
@@ -0,0 +1,22 @@
+export const Menuitems =[
+    {
+        title: 'people',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'places',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'practices',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Toggle',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
